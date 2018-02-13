19d3b5c90dcb2938f2e8f29c7d0d325e7bfdb310
Louisa Luo <louisa.luo3@gmail.com>
2018-02-12 20:06:25 -0800

    All posts listing home page

--- StudyBuddy/app/controllers/posts_controller.rb
+++ StudyBuddy/app/controllers/posts_controller.rb
@@ -1,7 +1,12 @@
 class PostsController < ApplicationController
 	def index
-		@posts = Post.all
+    if params[:course]
+      @posts = Post.search(params[:course])
+    else
+      @posts = Post.all
+    end
 	end
+
   def new
     @posts = Post.new 
   end
--- StudyBuddy/app/models/post.rb
+++ StudyBuddy/app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+
+  
+  def self.search(course)
+    if course
+      where('name LIKE ?', "%#{course}%")
+    else
+      all
+    end
+  end
 end
