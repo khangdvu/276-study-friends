7a526468352af8e3f64575719fad4fc93df413ae
Louisa Luo <louisa.luo3@gmail.com>
2018-02-15 16:59:20 -0800

    Syntax Fix

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,4 @@
-class Conversation < ActiveRecord::Base[5.1]
+class Conversation < ActiveRecord::Base
     belongs_to :sender, :foreign_key => :sender_id, class_name: ‘User’
     belongs_to :recipient, :foreign_key => :recipient_id, class_name: ‘User’
     has_many :messages, dependent: :destroy
