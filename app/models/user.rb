80170f0874f9ec6d59f64aaa4114150d1a928648
Christopher Lam <35906154+chrislam8@users.noreply.github.com>
2018-03-01 11:31:52 -0800

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 #validating inputs from signup page
   validates :name, :presence => true, :length => { :in => 3..20 }
   validates :email, :presence => true, :uniqueness => true
-  validates :password, :confirmation => true 		
+  validates :password, presence: true, length: {minimum: 6}	
     has_secure_password :validations => false
 
 end
