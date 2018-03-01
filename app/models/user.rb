2a04ed52d5f5afc7e3edf1aa2e35821f8f23a8ff
Christopher Lam <35906154+chrislam8@users.noreply.github.com>
2018-03-01 09:10:04 -0800

    Undo edit to user model
    Edit was to add column for an older version of rails (before 4.0)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessor :password
-  attr_accessible :availability
   
 #validating inputs from signup page
   validates :name, :presence => true, :length => { :in => 3..20 }
