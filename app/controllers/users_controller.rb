8a39393b30bbb20bc575fea1e86030affa126deb
Christopher Lam <35906154+chrislam8@users.noreply.github.com>
2018-03-04 15:27:57 -0800

    Require login to view user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   skip_before_filter :require_login
   
-before_action :logged_in_user, only: [:edit, :update]
+before_action :logged_in_user, only: [:show, :edit, :update]
   #before_action :correct_user, only: [:edit, :update]
 
   def new
