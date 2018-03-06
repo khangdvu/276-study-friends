512249c195a5b8dcc63b56532ef5f2c48f615000
Christopher Lam <35906154+chrislam8@users.noreply.github.com>
2018-03-05 17:13:09 -0800

    Add test for editing user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  
+  def setup
+    @user = User.new(name: 'Chris', email: 'test@test.ca')
+  end
+  test "user saved without email" do
+    @user.email = nil
+    refute @user.valid?, 'saved user without email'
+    assert_not_nil @users.errors[:email], 'no validation error for no email present'
+  end
+  
   # test "the truth" do
   #   assert true
   # end
