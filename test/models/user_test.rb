aa2ddf833f246d6f0a128e86cc81e8504b0fd058
Christopher Lam <35906154+chrislam8@users.noreply.github.com>
2018-03-07 16:41:19 -0800

    Add test for user model
    Test for not saving user with no password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,12 +3,18 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   def setup
-    @user = User.new(name: 'Chris', email: 'test@test.ca')
+    @user = User.new(name: 'Chris', email: 'test@test.ca', password: 'test34', password_confirmation: 'test34')
   end
   test "user saved without email" do
     @user.email = nil
     refute @user.valid?, 'saved user without email'
     assert_not_nil @users.errors[:email], 'no validation error for no email present'
+  end
+  
+  test "user saved without password" do
+    @uer.password = nil
+    refute @user.valid?, 'saved user without password'
+    assert_not_nil @users.errors[:password], 'no validation error for no password present'
   end
   
   # test "the truth" do
