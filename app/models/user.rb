4130c5a62f4159fcc906181fb0cd8aca137c575f
Khang <35905915+khangdvu@users.noreply.github.com>
2018-03-27 02:09:55 -0700

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,17 @@ class User < ActiveRecord::Base
 
 
   has_many :conversations, :foreign_key => :sender_id
-
+def self.create_with_omniauth(auth)
+  
+  user = find_or_create_by(uid: auth['uid'], provider:  auth['provider'])
+  user.email = "#{auth['uid']}@#{auth['provider']}.com"
+  user.password = auth['uid']
+  user.name = auth['info']['name']
+  if User.exists?(user)
+    user
+  else
+    user.save!
+    user
+  end
+end
 end
